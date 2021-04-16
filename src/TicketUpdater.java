72c6d8edfa3f49ffc219c9228b02e8bb1974a49c
bmalone2 <bmalone2@my.chattanoogastate.edu>
2021-04-16 18:45:22 -0400

    switch statement annihilation

--- src/TicketUpdater.java
+++ src/TicketUpdater.java
@@ -3,9 +3,19 @@ import java.util.Scanner;
 public class TicketUpdater {
     private String makeChanges;
     private int changeSelection;
+    private Scanner in = new Scanner(System.in);
+    private Ticket ticket;
+    private Flight flight;
 
-    Scanner in = new Scanner(System.in);
-    public  void  changeLoop(Ticket ticket1,Flight chosenFlight){
+    public TicketUpdater(Ticket ticket1, Flight chosenFlight)
+    {
+        ticket = ticket1;
+        flight = chosenFlight;
+    }
+
+
+
+    public  void  changeLoop(){
         do {
             makeChanges = setMakeChanges();
             if(makeChanges.trim().equalsIgnoreCase("n")){
@@ -13,18 +23,26 @@ public class TicketUpdater {
             }
             print();
             int userSelection = getSelection();
-            cases(ticket1, chosenFlight, userSelection);
-            if (userSelection == 3) {
+            if(userSelection == 1)
+            {
+                updateName();
+            }
+            else if (userSelection == 2)
+            {
+                changeSeat();
+            }
+            else if (userSelection == 3) {
+                cancelTicket();
                 break;
             }
         } while(makeChanges.trim().equalsIgnoreCase("y"));
     }
 
-	private String setMakeChanges(){
-		System.out.print("Would you like to make any changes to your reservation? (Y/N)");
-		makeChanges = in.next();
-		return makeChanges;
-	}
+    private String setMakeChanges(){
+        System.out.print("Would you like to make any changes to your reservation? (Y/N)");
+        makeChanges = in.next();
+        return makeChanges;
+    }
     private void print(){
         System.out.println("What would you like to change on your ticket?");
         System.out.println("Press 1 to change the name on the ticket");
@@ -39,44 +57,36 @@ public class TicketUpdater {
         return changeSelection;
     }
 
-
-    public void cases(Ticket ticket1, Flight chosenFlight, int changeSelection){
-        switch (changeSelection) {
-            case 1 : {
-                System.out.print("Enter a new name: ");
-                String name = in.next();
-                ticket1.setName(name);
-                chosenFlight.reserveSeat(ticket1.getSeatNum(), name);
-                System.out.println();
-                System.out.println("Name has been successfully changed.");
-                ticket1.print();
-                break;
-            }
-            case 2 : {
-                chosenFlight.cancelSeatReservation(ticket1.getSeatNum());
-                chosenFlight.getOpenSeats();
-                System.out.print("Enter a new seat number: ");
-                int newSeat = in.nextInt();
-                ticket1.setSeatNum(newSeat);
-                chosenFlight.reserveSeat(newSeat, ticket1.getName());
-                //do we need another obj? 
-                Ticket ticket2 = new Ticket(chosenFlight, newSeat, ticket1.getName());
-                System.out.println();
-                System.out.println("Seat has been successfully changed.");
-                ticket2.print();
-                break;
-            }
-            case 3 : {
-                ticket1.cancelTicket();
-                System.out.println("---Ticket canceled---");
-                break;
-            }
-            default : System.out.println("No valid selection");
-        }
+    public void updateName()
+    {
+        System.out.print("Enter a new name: ");
+        String name = in.next();
+        ticket.setName(name);
+        flight.reserveSeat(ticket.getSeatNum(), name);
+        System.out.println();
+        System.out.println("Name has been successfully changed.");
+        ticket.print();
     }
-
-	public TicketUpdater(){
-
+    public void changeSeat()
+    {
+        flight.cancelSeatReservation(ticket.getSeatNum());
+        flight.getOpenSeats();
+        System.out.print("Enter a new seat number: ");
+        int newSeat = in.nextInt();
+        ticket.setSeatNum(newSeat);
+        flight.reserveSeat(newSeat, ticket.getName());
+        //do we need another obj?
+        Ticket ticket2 = new Ticket(flight, newSeat, ticket.getName());
+        System.out.println();
+        System.out.println("Seat has been successfully changed.");
+        ticket2.print();
+    }
+    public void cancelTicket()
+    {
+        ticket.cancelTicket();
+        System.out.println("---Ticket canceled---");
     }
 }
+
+
 
