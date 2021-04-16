89fbcba42ab04250633ada5f0f069cd2077df358
bmalone2 <bmalone2@my.chattanoogastate.edu>
2021-04-16 13:00:44 -0400

    change prompt fix

--- src/FlightReservation.java
+++ src/FlightReservation.java
@@ -44,12 +44,13 @@ public class FlightReservation {
 
             //-------------------
 
+            ticket.print();
 
             TicketUpdater ticketupdater = new TicketUpdater();
             //todo no seat validation
             ticketupdater.changeLoop(ticket, chosenFlight);
 
-            System.out.printf("%n ---Your ticket--- %n");
+            System.out.printf("%n ---Your final ticket--- %n");
             ticket.print();
             System.out.print("Would you like to book a new flight? (Y/N)");
             repeat = in.next();
