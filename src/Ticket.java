33917cbf16d0e3157030ad139e648852a1a2de06
cecilyholland <80843091+cecilyholland@users.noreply.github.com>
2021-04-21 09:35:06 -0400

    javadoc

--- src/Ticket.java
+++ src/Ticket.java
@@ -1,3 +1,6 @@
+/**
+* ticket class gets all info from other classes and returns a printed ticket
+*/
 public class Ticket {
    private String departureCity;
    private String arrivalCity;
@@ -8,7 +11,12 @@ public class Ticket {
    private double seatCharge;
    private String duration;
    private String classType;
-
+/**
+ *  takes in all preset info and prints a ticket for the customer
+ * @param chosenFlight user selected flight 
+ * @param seatNum user selected seat number
+ * @param name of user 
+ */
    public Ticket (Flight chosenFlight, int seatNum, String name){
         this.departureCity = chosenFlight.getSourceCity();
         this.arrivalCity = chosenFlight.getDestinationCity();
@@ -20,27 +28,51 @@ public class Ticket {
         this.duration = chosenFlight.getFlightTime();
         this.classType = chosenFlight.getSeatList().get(seatNum - 1).getClassType();
    }
+/**
+ * @param seatNum selection
+ */
    public void setSeatNum(int seatNum) {
         this.seatNum = seatNum;
     }
+/**
+ * @param name of customer
+ */
    public void setName(String name) {
         this.name = name;
     }
+/**
+ * @return departure city
+ */
    public String getDepartureCity(){
        return departureCity;
    }
+/**
+* @return arrival city
+*/  
    public String getArrivalCity(){
        return arrivalCity;
    }
+/**
+* @return flight ID
+*/
    public String getFlightID(){
        return flightId;
    }
+/**
+* @return seat number
+*/
    public int getSeatNum(){
        return seatNum;
    }
+/**
+* @return name of cutsomer
+*/
    public String getName(){
        return name;
    }
+/**
+* cancel ticket reset method
+*/
    public void cancelTicket(){
         this.departureCity = null;
         this.arrivalCity = null;
@@ -51,6 +83,9 @@ public class Ticket {
         this.duration = null;
         this.classType = null;
    }
+/**
+* ticket print method
+*/
    public void print() {
         //todo maybe add more info like airline ?
         System.out.println("");
