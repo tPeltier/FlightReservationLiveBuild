65014f7fa71d0359ede765da58c82695159e5d39
cecilyholland <80843091+cecilyholland@users.noreply.github.com>
2021-04-21 08:53:14 -0400

    javadoc update

--- src/BusinessClassSeat.java
+++ src/BusinessClassSeat.java
@@ -1,9 +1,16 @@
-
+/**
+ * extra fee class for business class flight
+ * calculates extra for business class
+ */
 public class BusinessClassSeat extends FlightSeat {
     public BusinessClassSeat() {
         classType = "Business Class";
         extraCharge = 150.25;
     }
+ /**
+ * print statement
+ * 
+ */
     public void classGreeting() {
         System.out.println("---Thank you for flying business class!---");
     }
