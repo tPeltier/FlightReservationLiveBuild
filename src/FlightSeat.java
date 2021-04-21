6c3c1d2c2ebf01c47aaed13d40a25af88c5978dd
cecilyholland <80843091+cecilyholland@users.noreply.github.com>
2021-04-21 09:44:54 -0400

    javadoc

--- src/FlightSeat.java
+++ src/FlightSeat.java
@@ -1,12 +1,17 @@
 import java.util.Random;
-
+/**
+ * class that randomly generates the flight seats available and then reserves the one the user chooses
+ * 
+ */
 class FlightSeat {
     private boolean available;
     private String seatType;
     protected String classType;
     protected double extraCharge;
     private String assignedCustomer;
-
+/**
+ * random generator for flight seats
+ */
     public FlightSeat() {
         available = true;
         classType = "Economy";
@@ -26,29 +31,53 @@ class FlightSeat {
                 break;
         }
     }
+/**
+ * @return available seats
+ */
     public boolean getAvailability() {
         return available;
     }
+ /**
+ * @param customerName reserve for customer ""
+ */
     public void reserveFor(String customerName) {
         assignedCustomer = customerName;
         available = false;
     }
+/**
+* cancel reservation method
+*/
     public void cancelReservation() {
         assignedCustomer = "No One";
         available = true;
     }
+/**
+* @return chosen seat type of customer
+*/
     public String getSeatType() {
         return seatType;
     }
+/**
+* @return name of customer
+*/
     public String getAssignedName() {
         return assignedCustomer;
     }
+/**
+* @return class type of seat
+*/
     public String getClassType() {
         return classType;
     }
+/**
+* @return extra charge if any for chosen flight class and seat
+*/
     public double getExtraCharge() {
         return extraCharge;
     }
+/**
+* print statement clarification for customer
+*/
     public void classGreeting() {
         System.out.println("---Thank you for flying economy!---");
     }
